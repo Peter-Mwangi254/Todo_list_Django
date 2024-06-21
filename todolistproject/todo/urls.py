4e1ba32d0ce5506b2dff3812281d4a6a2681f969
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-21 03:17:52 +0300

    Added todo views

--- todolistproject/todo/urls.py
+++ todolistproject/todo/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
-    path('task-create/', TaskCreate.as_view(), name="task-create"),
-    path('login/', CustomLoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
-    path('register/', RegisterPage.as_view(), name="register"),
+    # path('task-create/', TaskCreate.as_view(), name="task-create"),
+    # path('login/', CustomLoginView.as_view(), name="login"),
+    # path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    # path('register/', RegisterPage.as_view(), name="register"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="tasks-detail"),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name="tasks-update"),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete") 
+    # path('task-update/<int:pk>/', TaskUpdate.as_view(), name="tasks-update"),
+    # path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete") 
 ]
